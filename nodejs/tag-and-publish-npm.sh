65e52d4813e5f45fac21acfa44cd5d262c571c03
Martin Swiderski <martinswiderski@googlemail.com>
2017-03-23 04:04:31 -0700

    upd

--- nodejs/tag-and-publish-npm.sh
+++ nodejs/tag-and-publish-npm.sh
@@ -5,6 +5,6 @@ echo "| Tag and publish npm module |"
 echo "+----------------------------+"
 echo ""
 echo "Work in progress..."
-echo "This is just a placeholder."
+echo "and this is just a placeholder."
 echo ""
 exit 1
