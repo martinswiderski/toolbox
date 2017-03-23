8830d10691ab339cc105efe5879cea6edf134995
Martin Swiderski <martin.swiderski@gmail.com>
2017-03-23 06:29:06 -0700

    working

--- ui-selenium-test/reports/README.md
+++ ui-selenium-test/reports/README.md
@@ -1,5 +1,13 @@
 Test results container
 --
 
+In order to run tests with captured output go to top project directory and execute:
+
+```
+npm test
+```
+
+
 Tests run and results: 
 
+ * [Tests run (by ) on Thu Mar 23 06:27:36 PDT 2017](20170323-0627-36.md)
--- ui-selenium-test/src/config.js
+++ ui-selenium-test/src/config.js
@@ -1,25 +1,33 @@
 'use strict';
 
-let webdriver = require('selenium-webdriver');
-let By = webdriver.By;
-let until = webdriver.until;
-
-var proxy = require('selenium-webdriver/proxy');
-let driver = new webdriver.Builder()
-    .withCapabilities({ 'browserName': 'firefox' })
-    .setProxy(proxy.manual({
-        http: 'proxy.scee.net:3128',
-        https: 'proxy.scee.net:3128'
-    }))
-    .build();
-
-var path = require('path'),
+var i = 0, _instances = {},
+    path = require('path'),
     fs = require('fs'),
     md5 = require('md5');
 
 module.exports = {
-    webdriver: webdriver,
-    driver: driver,
+    browser: function () {
+        ++i;
+        var webdriver = require('selenium-webdriver'),
+            By = webdriver.By,
+            until = webdriver.until,
+            proxy = require('selenium-webdriver/proxy'),
+            driver = new webdriver.Builder()
+            .withCapabilities({ 'browserName': 'firefox' })
+            .setProxy(proxy.manual({
+                http: 'proxy.scee.net:3128',
+                https: 'proxy.scee.net:3128'
+            })).build();
+
+        return {
+            webdriver: webdriver,
+            By: By,
+            until: until,
+            proxy: proxy,
+            webdriver: webdriver,
+            driver: driver
+        };
+    },
     signFile: function (file) {
         console.log( 'MD5: ' + md5(fs.readFileSync(file)) + ' File: ' + path.basename(file) );
     }
