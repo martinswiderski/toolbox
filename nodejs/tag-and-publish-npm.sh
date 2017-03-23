adc21c6680551ff23370fd076b97c1d876b9f8d4
Martin Swiderski <codebloke@gmail.com>
2017-03-23 07:39:40 +0000

    more

--- nodejs/tag-and-publish-npm.sh
+++ nodejs/tag-and-publish-npm.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Placeholder"
-exit 0
+clear
+echo "+----------------------------+"
+echo "| Tag and publish npm module |"
+echo "+----------------------------+"
+echo ""
+echo "Work in progress..."
+echo "This ix just a placeholder."
+echo ""
+exit 1
