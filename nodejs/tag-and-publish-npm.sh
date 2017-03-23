fdf570eb8bb3463d998c8ab12e1056bcbfdad81b
Martin Swiderski <codebloke@gmail.com>
2017-03-23 07:44:14 +0000

    wip

--- nodejs/tag-and-publish-npm.sh
+++ nodejs/tag-and-publish-npm.sh
@@ -1,10 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
 clear
-echo "+----------------------------+"
-echo "| Tag and publish npm module |"
-echo "+----------------------------+"
+NL="\n"
+CURRENT_NPM_VER=`npm version | grep '{' | cut -d "'" -f 2`
+CURRENT_VCS=`cat package.json | grep '\.git' | cut -d '"' -f 4 | cut -d '+' -f 2`
+CURRENT_MAINTAINER=`npm v | grep maintainers | cut -d "'" -f 2`
+CURRENT_BRANCH=`git branch | grep "*" | cut -d " " -f 2`
+CURRENT_WIP=`git status | grep "modified\|renamed"`
+echo "+---------------------------+"
+echo "| Release management script |"
+echo "+---------------------------+"
 echo ""
-echo "Work in progress..."
-echo "This ix just a placeholder."
+echo "Maintainer          : $CURRENT_MAINTAINER"
+echo "Current version     : $CURRENT_NPM_VER"
+echo "VCS                 : $CURRENT_VCS"
+echo "Branch              : $CURRENT_BRANCH"
+echo "Uncommitted WIP     : "
+git status | grep "modified\|renamed"
 echo ""
-exit 1
+echo "---"
+echo "login to npm..."
+echo ""
+#npm login
+echo ""
+echo "---"
+echo "What is the version that you want to release?"
+read RELEASE_VER
+VER=`node src/tools/version.js --check $RELEASE_VER $CURRENT_NPM_VER`
+if [ "$VER" == "Invalid version" ]
+then
+    echo "ERROR: $VER '$RELEASE_VER'";
+    exit 1
+fi
+if [ "$VER" == "Wrong version number" ]
+then
+    echo "ERROR: Wrong version number: $RELEASE_VER (existing: $CURRENT_NPM_VER)";
+    exit 1
+fi
